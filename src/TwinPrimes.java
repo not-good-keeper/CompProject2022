4e5c95551443cb4cc69a17f313e865dc8fd0918f
not-good-keeper <115866691+not-good-keeper@users.noreply.github.com>
2022-10-15 13:38:10 +0530

    Update TwinPrimes.java

--- src/TwinPrimes.java
+++ src/TwinPrimes.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 // PROGRAM 3
 
 /*
+Question 3
+Twins primes are consecutive prime numbers whose difference is 2. For example, (3,5), (11,13), (17,19) are all twin primes. We define the distance of any twin prime pair from a positive integer as follows. If ( p 1,p2) is a twin prime pair and n is a positive integer then the distance of the twin prime from n is: minimum(abs(n-pl), abs(n-p2)) where abs returns the absolute value of its argument, and minimum returns the smaller of its two arguments. Write a program that reads in a positive integer n and prints out the twin prime pair that has the least distance from n.
+For example:
+
+(a) if n is 30 then the pair is (29, 31),
+(b) if n is 13 it is (11,13), if n is 49 it is(41, 43)
+(c) if n is 54 it is (59, 61).
+Sample Input: 34
+Sample Output:
+Number read in is 34              P1 = 59, P2 = 61
+Sample Input: 60
+Sample Output:
+Number read in is 60              P1 = 29, P2= 31
+
 Algorithm:
 1. Start
 2. Input a number
