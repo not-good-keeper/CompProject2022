8b53371ec849f5fedaf4a68a0c6f86b6ac3d83fe
not-good-keeper <115866691+not-good-keeper@users.noreply.github.com>
2022-10-15 13:35:14 +0530

    Update SmithNumber.java

--- src/SmithNumber.java
+++ src/SmithNumber.java
@@ -1,3 +1,19 @@
+/*
+Question 1
+A Smith number is a composite number, whose sum of the digits is equal to the sum of its prime factors.
+For example:
+22, 27, 58, 85, 94, 121…………….....are Smith numbers.
+Write a program in Java to enter a number and check whether it is a Smith number or not.
+
+Sample Input: 666
+Sum of the digits: 6 + 6 + 6 = 18
+Prime factors are: 2, 3, 3, 37
+Sum of the digits of the prime factors: 2 + 3 + 3 + (3 + 7) = 18 
+Thus, 666 is a Smith Number.
+
+*/
+
+
 import java.util.Scanner;
 
 // PROGRAM 1
